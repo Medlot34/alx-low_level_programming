d4066494835b9caa1c4f824c0b956a22185953d6
medlot34 <mohamed.lq34@gmail.com>
2023-07-19 02:54:06 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*Discription : 'check for number'
+*Return: always 0
+*/
+int main(void)
+{
+
+int n;
+for (n = 0; n <= 9; n++)
+{
+printf("%d", n);
+}
+putchar('\n');
+return (0);
+}
