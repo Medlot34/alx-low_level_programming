9e9b0284bace42879953bb5c0655dc2e2ac275f8
medlot34 <mohamed.lq34@gmail.com>
2023-07-19 04:25:04 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*Discription : 'check for number'
+*Return: always 0
+*/
+int main(void)
+{
+
+int n = 97;
+
+while (n >= 97)
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+return (0);
+}
