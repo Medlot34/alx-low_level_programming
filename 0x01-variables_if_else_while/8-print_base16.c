3a2471f16f7cedb23f164f239f39040e8771f349
medlot34 <mohamed.lq34@gmail.com>
2023-07-19 04:34:26 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*Discription : 'check for number'
+*Return: always 0
+*/
+int main(void)
+{
+
+int n;
+int m;
+
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+for (m = 97; n <= 102; m++)
+{
+putchar(m);
+}
+putchar('\n');
+return (0);
+}
