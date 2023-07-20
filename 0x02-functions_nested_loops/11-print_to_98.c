bef9eb8da1dab8ff69da16662cb05c6044940eef
medlot34 <mohamed.lq34@gmail.com>
2023-07-20 16:24:24 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_to_98 - all numbers
+*@n: first number
+*Return: no return
+*/
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+for (n = n; n <= 97; n++)
+printf("%d, ", n);
+}
+else
+for (n = n; n > 98; n--)
+printf("d, ", n);
+printf("98\n");
+}
