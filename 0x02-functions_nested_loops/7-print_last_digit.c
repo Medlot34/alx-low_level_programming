1ff7dca2c43dcfe3bdc17f344d805a273a50fdd8
medlot34 <mohamed.lq34@gmail.com>
2023-07-20 10:22:43 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,21 +3,18 @@
 *@n : imput
 *Return: the value of the last digit
 */
-int print_last_digit(int)
+int print_last_digit(int n)
 {
 
 int l;
-int n;
 
 l = n % 10;
 if (l < 0)
 {
-_putchar(-l + 48);
 return (-l);
 }
 else
 {
-_putchar(l + 48);
 return (l);
 }
 }
